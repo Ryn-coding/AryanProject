406c1ed0132d123458e3a7019b483e4d6196a25f
Aryan <108799012+Ryn-coding@users.noreply.github.com>
2022-07-08 11:17:23 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+import { UserContainer } from "./UserContainer";
+
+export default function App() {
+
+  return (
+
+    <div className="App">
+
+      <h1>Hello CodeSandbox</h1>
+
+      <UserContainer />
+
+    </div>
+
+  );
+
+}
+
